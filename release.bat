520aae5d16a8841159d295cef311f442ed58157e
Jacek Nowacki <git-public@dzek.eu>
2018-02-17 23:28:05 +0100

    - remove old releases files on building releases

--- release.bat
+++ release.bat
@@ -1,3 +1,4 @@
 @echo off
+del /q releases\*
 call npx pkg . --targets node9-win-x64,node9-win-x86 --out-path releases/
 call ren releases\docker-windows-notifier-x64.exe docker-windows-notifier.exe
